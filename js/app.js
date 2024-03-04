102d4a825f42aa99ce8a0f85af2120463c50933e
F Bliss <60014136+voffee@users.noreply.github.com>
2024-03-04 21:50:19 +0800

    Flipping CSS complete + positioning complete

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
 
-// alert(10);
+const cardFlip = document.querySelector('.voucher-container');
+
+cardFlip.addEventListener('click', function () {
+    cardFlip.classList.toggle('is-flipped');
+});
