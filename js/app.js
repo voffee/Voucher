7db8c3731b1c41de33303d7365ead297577d3c23
F Bliss <60014136+voffee@users.noreply.github.com>
2024-03-08 20:05:26 +0800

    Seperated voucher from unwrapper.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 
+// Declaring Variables
 const cardFlip = document.querySelector('.voucher-container');
+const unWrap = document.querySelector('.unwrapper-container');
 
-cardFlip.addEventListener('click', function () {
+
+// Creating Event Listener
+cardFlip.addEventListener('click', function() {
     cardFlip.classList.toggle('is-flipped');
 });
+
+// Creating Function
+function unwrapVoucher() {
+
+}
