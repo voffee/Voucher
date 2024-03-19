def5d6d30e948c50f99e6f9a112512cc93355288
F Bliss <60014136+voffee@users.noreply.github.com>
2024-03-19 23:06:25 +0800

    Created JS to toggle CSS classes & added Media Query (Basic)

--- js/app.js
+++ js/app.js
@@ -2,14 +2,26 @@
 // Declaring Variables
 const cardFlip = document.querySelector('.voucher-container');
 const unWrap = document.querySelector('.unwrapper-container');
+const wrapperActual = document.querySelector('.voucher-unwrap');
 
 
-// Creating Event Listener
+// Creating Event Listeners
 cardFlip.addEventListener('click', function() {
     cardFlip.classList.toggle('is-flipped');
 });
 
+unWrap.addEventListener('click', unwrapVoucher);
+
 // Creating Function
 function unwrapVoucher() {
 
+    if(cardFlip.classList.contains('unwrap')) {
+        cardFlip.classList.remove('unwrap');
+        wrapperActual.classList.add('button-transform');
+    }
+
+    else {
+        cardFlip.classList.add('unwrap');
+        wrapperActual.classList.remove('button-transform');
+    }
 }
